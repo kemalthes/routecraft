91e6829786c339ba50da96c3277f0e82480bf9c2
kemalthes <stop1qq@gmail.com>
2026-04-27 00:01:28 +0300

    add openapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.4.0"
 }
 
 group = "io.kemalthes"
@@ -18,7 +19,16 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    main {
+        java {
+            srcDir(layout.buildDirectory.dir("generated-openapi/src/main/java"))
+        }
+    }
+}
+
 val openapiVersion = "2.6.0"
+val jacksonNullableVersion = "0.2.6"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-amqp")
@@ -29,12 +39,42 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.liquibase:liquibase-core")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$openapiVersion")
+    implementation("org.openapitools:jackson-databind-nullable:${jacksonNullableVersion}")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+openApiGenerate {
+    generatorName.set("spring")
+    inputSpec.set("$projectDir/src/main/resources/openapi/api-core-v1.yml")
+    outputDir.set(layout.buildDirectory.dir("generated-openapi").get().asFile.path)
+    apiPackage.set("io.kemalthes.core.api")
+    modelPackage.set("io.kemalthes.core.dto")
+    typeMappings.set(mapOf("URI" to "String"))
+    globalProperties.set(mapOf(
+        "apis" to "",
+        "models" to "",
+        "apiTests" to "false",
+        "modelTests" to "false",
+        "supportingFiles" to "ApiUtil.java"
+    ))
+    configOptions.set(mapOf(
+        "interfaceOnly" to "true",
+        "useTags" to "true",
+        "useSpringBoot3" to "true",
+        "generateBuilders" to "true",
+        "useBeanValidation" to "true",
+        "performBeanValidation" to "true",
+        "useJakartaEe" to "true"
+    ))
+}
+
+tasks.compileJava {
+    dependsOn(tasks.openApiGenerate)
 }
 
 tasks.withType<Test> {
